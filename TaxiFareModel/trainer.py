e43e5656bd43131822a2220acf4273c7b554cae1
William Calderon <wecalderonc@unal.edu.co>
2021-11-16 16:32:29 -0300

    connect to mlflow lewagon

--- TaxiFareModel/trainer.py
+++ TaxiFareModel/trainer.py
@@ -7,6 +7,12 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.linear_model import LinearRegression
+from memoized_property import memoized_property
+
+import mlflow
+from mlflow.tracking import MlflowClient
+
+MLFLOW_URI = "https://mlflow.lewagon.co/"
 
 class Trainer():
     def __init__(self, X, y):
@@ -19,6 +25,8 @@ class Trainer():
         self.y = y
         self.X_train = None
         self.y_train = None
+        self.experiment_name = "[BR] [RJ] [wecalderonc] Linear 1.0.0"
+
 
     def set_pipeline(self):
         '''returns a pipelined model'''
@@ -54,8 +62,31 @@ class Trainer():
         '''returns the value of the RMSE'''
         y_pred = self.pipeline.predict(X_test)
         rmse = compute_rmse(y_pred, y_test)
-        print(rmse)
+        #self.mlflow_log_param(param_name, param_value)
+        self.mlflow_log_metric("rmse", rmse)
         return rmse
+
+    @memoized_property
+    def mlflow_client(self):
+        mlflow.set_tracking_uri(MLFLOW_URI)
+        return MlflowClient()
+
+    @memoized_property
+    def mlflow_experiment_id(self):
+        try:
+            return self.mlflow_client.create_experiment(self.experiment_name)
+        except BaseException:
+            return self.mlflow_client.get_experiment_by_name(self.experiment_name).experiment_id
+
+    @memoized_property
+    def mlflow_run(self):
+        return self.mlflow_client.create_run(self.mlflow_experiment_id)
+
+    def mlflow_log_param(self, key, value):
+        self.mlflow_client.log_param(self.mlflow_run.info.run_id, key, value)
+
+    def mlflow_log_metric(self, key, value):
+        self.mlflow_client.log_metric(self.mlflow_run.info.run_id, key, value)
 
 
 if __name__ == "__main__":
@@ -66,11 +97,13 @@ if __name__ == "__main__":
     # set X # set X and y
     y = df["fare_amount"]
     X = df.drop("fare_amount", axis=1)
-
     # hold out
     # train
     trainer = Trainer(X, y)
     trainer.run()
     # evaluate
     rmse = trainer.evaluate(trainer.X_test, trainer.y_test)
+    experiment_id = trainer.mlflow_experiment_id
+
+    print(f"experiment URL: https://mlflow.lewagon.co/#/experiments/{experiment_id}")
     print(rmse)
